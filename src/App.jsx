d63a417c527fc31fc5135c18c78aef5ebf3793bc
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-01-23 17:50:59 -0500

    added Category Page, moved category components into category folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { useSelector } from "react-redux"
 import Header from './components/Header'
 import MainPage from './components/MainPage'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import ProfilePage from './components/profile/ProfilePage'
+import CategoryPage from './components/category/CategoryPage'
 
 function App() {
-
+  const selectedCategory = useSelector((state) => state.category.value)
+  console.log(selectedCategory)
   return (
     <div className="App">
       <Header />
@@ -16,6 +19,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path={`/category/${selectedCategory.category}`} element={<CategoryPage />}/>
       </Routes>
     </div>
   )
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,10 @@
-import CategoryCard from "./CategoryCard"
-import { useSelector } from "react-redux"
+import CategoryCard from "./category/CategoryCard"
 
 const MainPage = () => {
-    const selectedCategory = useSelector((state) => state.category.value)
     const categories = ["all", "beer", "brandy", "gin", "rum", "tequila", "vodka", "whiskey", "wine", "other"]
 
     return (
         <div className="main-page-container">
-            <h1>{selectedCategory.category}</h1>
             <div className="main-content">
                 {
                     categories.map((category) => {
